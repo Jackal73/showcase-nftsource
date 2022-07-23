4eca294a906d7cb769b0b75230c29d8130d0883d
Shawn Kebel <fxrender@outlook.com>
2022-07-23 11:38:23 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import assets from "./assets";
+import { Download, Features, SectionWrapper } from "./components";
+import styles from "./styles/Global";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper
+        title="Your own store of cool NFTs. Start Selling and Growing!"
+        description="Buy, store, and collect NFTs. Exchange and earn crypto. Join 2+ million people using NFTSource Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface, Incredible User Experience"
+        description="Indulge in the buttery UI of NFTsource - NFT Marketplace. The smooth, constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="NFTsource is built using Expo, which runs natively on all user devices. This allows you to easily deliver your app into people's hands."
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase your store"
+        description="This app contains two screens. The first screen lists all your NFTs, while the second shows the details of a specific."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with UI/UX in mind by {" "}
+        <a href="https://shawnkebel-portfolio.netlify.app/"><span className="bold hover:text-tertiary">ShawnKebel@kblDesigners</span></a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
